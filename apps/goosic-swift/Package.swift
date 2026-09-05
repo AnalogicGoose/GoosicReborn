283a12c264334a03d8995185b307aa763d68fa30
Bismarck Flores <gansolandia123@gmail.com>
2026-09-05 17:14:37 -0600

    Name the Swift package after the executable it produces
    The manifest called the package GoosicSwift while the product, the directory,
    and everything referring to it from outside call it goosic-swift. Terminate the
    file with a newline while here.

--- apps/goosic-swift/Package.swift
+++ apps/goosic-swift/Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoosicSwift",
+    name: "goosic-swift",
     platforms: [.macOS(.v14)],
     products: [
         .executable(name: "goosic-swift", targets: ["GoosicSwift"]),
